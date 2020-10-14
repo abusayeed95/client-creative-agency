a3f2955d7c18379047364533768738422629a7e9
ahmedmusa995 <sayeedsayem8@gmail.com>
2020-10-14 17:12:55 +0600

    working

--- src/Components/HomePage/OurServices/OurServices.js
+++ src/Components/HomePage/OurServices/OurServices.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Spinner } from 'react-bootstrap';
 import ServiceBox from '../ServiceBox/ServiceBox';
 import './OurServices.css'
 
@@ -36,21 +36,29 @@ const OurServices = () => {
     const shuffle = services.sort(() => .5 - Math.random());
     const shuffled6Services = shuffle.slice(0, 6) || shuffle;
 
+
     return (
-        <div className="service-area d-flex align-items-center">
-            <div>
-                <h2 className="text-center navy-blue-text font-weight-bold my-5 pt-5">
-                    Provide Awesome <span className="green-text">Services</span>
-                </h2>
-                <div className="service-container ">
-                    <Row className="m-0">
-                        {
-                            shuffled6Services.map(service => <ServiceBox key={service._id} service={service} />)
-                        }
-                    </Row>
-                </div>
-            </div>
-        </div>
+        <>
+            {
+                shuffled6Services.length > 0 ?
+                    <div className="service-area d-flex align-items-center">
+                        <div>
+                            <h2 className="text-center navy-blue-text font-weight-bold my-5 pt-5">
+                                Provide Awesome <span className="green-text">Services</span>
+                            </h2>
+                            <div className="service-container ">
+                                <Row className="m-0">
+                                    {
+                                        shuffled6Services.map(service => <ServiceBox key={service._id} service={service} />)
+                                    }
+                                </Row>
+                            </div>
+                        </div>
+                    </div> :
+                    <Spinner animation="border" variant="success" />
+            }
+        </>
+
     );
 };
 
